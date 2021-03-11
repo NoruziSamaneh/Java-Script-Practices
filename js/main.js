312142900dbaab4ffef184a0409e5f3eaccc2069
NoruziSamaneh <noruzi.samaneh793@gmail.com>
2021-03-11 16:32:06 +0330

    session 1 practice

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const firstName = 'Samaneh';
+let age = 29;
+const nationalCode = '123456789';
+let job = 'Security expert'
+
+function getName() {
+    console.log(`My name is: ${firstName}`);
+}
+
+function getAge() {
+    console.log(`I'm ${age} years old`);
+}
+
+function getNationalCode() {
+    console.log(`I's ${nationalCode}`);
+}
+
+function getJob() {
+    console.log(`I'm a ${job}`);
+}
